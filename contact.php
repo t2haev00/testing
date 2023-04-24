b12c7d5042da7e559b3db29df5d68c2caf22a8ac
t2haev00 <eveliinahampus@gmail.com>
2023-04-24 20:15:21 +0300

    Push

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("t2haev00@students.oamk.fi", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
